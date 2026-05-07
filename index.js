bc15ec49283a3b57c5e8fe0b8a1f8b85bcf21d52
kovendanragu15 <kovendanragu15@gmail.com>
2026-05-07 12:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Azure! </h1><p>Kovendan Node.js app is live!</p>');
+  res.send(`
+    <h1>Hello from Azure! 🚀</h1>
+    <p><b>NODE_ENV:</b> ${process.env.NODE_ENV}</p>
+    <p><b>PORT:</b> ${process.env.PORT}</p>
+    <p><b>App is running correctly!</b></p>
+  `);
 });
 
 app.listen(port, () => {
